6252678fbf8f15062c9156d6c877a5b2367b0476
Rhysand <jnhdenver@gmx.com>
2019-11-18 08:31:45 -0700

    Added LoadFile Function

--- Week12-utility.py
+++ Week12-utility.py
@@ -5,3 +5,14 @@
 def PrintOutput(string):
     print("OUTPUT", string)
 
+def LoadFile(string):
+    list_of_lines = []
+    with open (string, 'r') as file:
+        for line in file:
+            line_temp = line
+            list_of_lines.append(line_temp)
+    return PrintOutput(list_of_lines)
+        
+
+
+
